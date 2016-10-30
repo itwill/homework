8a29abcdd46fa12f7cf70f31a2141f305c01fe5e
Svetlana Chayka <probnii@mail.ru>
2016-10-31 00:12:23 +0500

    Исправление синтаксиса

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ echo "<h3>Задание 1</h3><br>";
 
 $name = "Svetlana";
 $age = "100";
-echo "Меня зовут " . $name."<br>";
+echo "Меня зовут " . $name . "<br>";
 echo "Мне $age лет<br>";
 echo "\" двойная кавычка<br>";
 echo "! воскл. знак<br>";
@@ -27,11 +27,11 @@ echo "<h3>Задание 2</h3><br>";
 $sum = 80;
 $a = 23;
 $b = 40;
-$c = $sum - $a -$b;
-echo "Всего рисунков: ".$sum."<br>";
-echo "Фломастерами выполнено: ".$a."<br>";
-echo "Карандашами: ".$b."<br>";
-echo "Красками на школьной выставке выполнено: <b>".$c."</b> рисунков";
+$c = $sum - $a - $b;
+echo "Всего рисунков: " . $sum . "<br>";
+echo "Фломастерами выполнено: " . $a . "<br>";
+echo "Карандашами: " . $b . "<br>";
+echo "Красками на школьной выставке выполнено: <b>" . $c . "</b> рисунков";
 
 
 echo "<h3>Задание 3</h3><br>";
@@ -42,7 +42,7 @@ define("MORNING", NULL);
 if (defined("MORNING") == true) echo "Константа объявлена<br>";
 else echo "Константа не объявлена<br>";
 
-echo "Константа MORNING = ".MORNING."<br>";
+echo "Константа MORNING = " . MORNING . "<br>";
 
 echo "Меняем значение константы на Утро и выводим:<br>";
 define("MORNING", "Утро");
@@ -52,13 +52,13 @@ echo MORNING;
 echo "<h3>Задание 4</h3><br>";
 
 $age = 15;
-echo "Возраст ".$age." лет<br>";
+echo "Возраст " . $age . " лет<br>";
 
-if (($age>=18)&&($age <= 65)):
+if (($age >= 18) && ($age <= 65)):
     echo "Вам еще работать и работать";
 elseif ($age > 65):
     echo "Вам пора на пенсию";
-elseif (($age >= 1) && ( $age <= 17)):
+elseif (($age >= 1) && ($age <= 17)):
     echo "Вам еще рано работать";
 else:
     echo "<br>Неизвестный возраст";
@@ -66,45 +66,42 @@ endif;
 
 echo "<h3>Задание 5</h3><br>";
 $day = 20;
-echo "День: ".$day."<br>";
+echo "День: " . $day . "<br>";
 switch ($day) {
-    case (($day < 1) || ($day > 7)) :
-    {
+    case (($day < 1) || ($day > 7)) : {
         echo "Неизвестный день";
         break;
     }
-    case (($day >= 1) && ($day <= 5)) :
-        {
-            echo "Это рабочий день";
-            break;
-        }
-    case (($day = 6) || ($day = 7)) :
-        {
-            echo "Это выходной день";
-            break;
-        }
+    case (($day >= 1) && ($day <= 5)) : {
+        echo "Это рабочий день";
+        break;
+    }
+    case (($day = 6) || ($day = 7)) : {
+        echo "Это выходной день";
+        break;
+    }
 }
 
 echo "<h3>Задание 6</h3><br>";
 $bmv = array(
     "model" => "X5",
-    "speed"=>"120",
-    "doors"=>"5",
-    "year"=>"2015",
+    "speed" => "120",
+    "doors" => "5",
+    "year" => "2015",
 );
 
 $toyota = array(
     "model" => "prado",
-    "speed"=>"160",
-    "doors"=>"5",
-    "year"=>"2014",
+    "speed" => "160",
+    "doors" => "5",
+    "year" => "2014",
 );
 
 $opel = array(
     "model" => "astra",
-    "speed"=>"80",
-    "doors"=>"5",
-    "year"=>"2008",
+    "speed" => "80",
+    "doors" => "5",
+    "year" => "2008",
 );
 
 $auto = array(
@@ -138,9 +135,10 @@ echo $str . "<br>";
 $str_arr = explode(" ", $str);
 print_r($str_arr);
 //var_dump($new_str);
+
 $new_str_arr = [];
 $kol = count($str_arr);// всего элементов
-$k = $kol-1; // номер последнего элемента для цикла
+$k = $kol - 1; // номер последнего элемента для цикла
 do {
     $new_str_arr[] = $str_arr[$k];
     $k--;
